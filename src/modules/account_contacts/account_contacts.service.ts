3c710aad97d34548e2738ae4028b5d0539e7130e
ScarletteAA <scarlette.araya@alumnos.ucn.cl>
2024-06-16 00:11:58 -0400

    chore: Update AccountContactsService

--- src/modules/account_contacts/account_contacts.service.ts
+++ src/modules/account_contacts/account_contacts.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAccountContactDto } from './dto/create-account_contact.dto';
 import { UpdateAccountContactDto } from './dto/update-account_contact.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AccountContactsService {
+  constructor(
+    private prisma: PrismaService,
+  ) {}
+
   create(createAccountContactDto: CreateAccountContactDto) {
-    return 'This action adds a new accountContact';
+    return this.prisma.account_contacts.create({
+      data:{
+        contacts: {
+          connect:{
+            id: createAccountContactDto.contact_id,
+          }
+        },
+
+        bank_accounts: {
+          connect:{
+            id: createAccountContactDto.account_id,
+          }
+        }
+      }
+    });
   }
 
   findAll() {
-    return `This action returns all accountContacts`;
+    return this.prisma.account_contacts.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} accountContact`;
+  findOne(id: string) {
+    return this.prisma.account_contacts.findUnique({
+      where:{
+        id:id,
+      }
+    });
   }
 
   update(id: number, updateAccountContactDto: UpdateAccountContactDto) {
     return `This action updates a #${id} accountContact`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} accountContact`;
+  remove(id: string) {
+    return this.prisma.account_contacts.delete({
+      where:{
+        id:id,
+      }
+    });
   }
 }
