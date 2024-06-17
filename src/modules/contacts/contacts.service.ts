69ff918d6c77a4d62da1a71125de674f55324a0c
WindWaker991 <seba.avergara@gmail.com>
2024-06-16 21:34:36 -0400

    chore: Refactor ContactsController and ContactsService

--- src/modules/contacts/contacts.service.ts
+++ src/modules/contacts/contacts.service.ts
@@ -6,60 +6,60 @@ import { Contact } from 'src/interfaces/contact.interface';
 
 @Injectable()
 export class ContactsService {
-  constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-  async create(createContactDto: CreateContactDto): Promise<Contact> {
-    const response = await this.prisma.contact.create({
+  async create(createContactDto: CreateContactDto) {
+    const response = await this.prisma.contacts.create({
       data: {
         id: createContactDto.id,
-        bankAccount: {
-          connect: { id: createContactDto.bankAccount.id }
-        }
+        bank_account: {
+          connect: { id: createContactDto.bankAccount.id },
+        },
       },
-      include: { bankAccount: true }
+      include: { bank_account: true },
     });
-    if (!response.bankAccount){
-      console.log("No se ha podido crear el contacto");
+    if (!response) {
+      console.log('No se ha podido crear el contacto');
       return;
     }
     return response;
   }
 
-  async findAll(): Promise<Contact[]> {
-    return await this.prisma.contact.findMany({
-      include: { bankAccount: true }
+  async findAll() {
+    return await this.prisma.contacts.findMany({
+      include: { bank_account: true },
     });
   }
 
-  async findOne(id: number): Promise<Contact> {
-    return await this.prisma.contact.findUnique({
+  async findOne(id: string) {
+    return await this.prisma.contacts.findUnique({
       where: { id: id },
-      include: { bankAccount: true }
+      include: { bank_account: true },
     });
   }
 
-  async update(id: number, updateContactDto: UpdateContactDto): Promise<Contact> {
-    return await this.prisma.contact.update({
+  async update(id: string, updateContactDto: UpdateContactDto) {
+    return await this.prisma.contacts.update({
       where: { id: id },
       data: {
         id: updateContactDto.id,
-        bankAccount: {
-          connect: { id: updateContactDto.bankAccount.id }
-        }
+        bank_account: {
+          connect: { id: updateContactDto.bankAccount.id },
+        },
       },
-      include: { bankAccount: true }
+      include: { bank_account: true },
     });
   }
 
-  async remove(id: number): Promise<Contact> {
-    const contact = this.findOne(id);
-    if (!contact){
-      console.log("No se ha podido encontrar el contacto");
+  async remove(id: string) {
+    const contact = this.prisma.contacts.findUnique({ where: { id: id } });
+    if (!contact) {
+      console.log('No se ha podido encontrar el contacto');
       return;
     }
-    await this.prisma.contact.delete({
+    await this.prisma.contacts.delete({
       where: { id: id },
-      include: { bankAccount : true }
+      include: { bank_account: true },
     });
 
     /** // No estoy seguro si este código sea necesario, por la cuenta del banco.
